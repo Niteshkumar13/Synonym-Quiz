83b2be90c87852fda35fbcb4b4093eabe929e68f
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-10-17 09:36:40 +0530

    update settings.py

--- quiz1/settings.py
+++ quiz1/settings.py
@@ -107,7 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-STATICFILES_DIRS = [BASE_DIR,'staticfile']
+STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','staticfile')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
